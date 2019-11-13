bd517e84f63a6d51d4232bf0e9fa7ddcab03559b
MenMik <mikolaj.mendala@lo1.sandomierz.pl>
2019-11-13 11:43:28 +0100

    petla

---
+++ python/petla.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def sumuj_parzyste():
+    suma = 0
+    for i in range(0,101,2):
+       # liczba = int(intput("podaj liczbę:"))
+        suma = suma + i
+        
+    print(suma)   
+
+
+
+def main(args):
+    sumuj_parzyste
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

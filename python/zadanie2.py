17487c0dbbe28ff0cc660dd84de164bffd8f3e0b
MenMik <mikolaj.mendala@lo1.sandomierz.pl>
2019-11-21 12:35:19 +0100

    zadania python

--- python/zadanie2.py
+++ python/zadanie2.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def main(args):
-    liczba = int(input("Podaj liczbę m: "))
-    liczba = int(input("Podaj liczbę n: "))
-    for 
-    
+    m = n = 0
+    while m < 1:
+        m = int(input("Podaj liczbę m: "))
+    while n < 1 or n < m :
+        n = int(input("Podaj liczbę n: "))
+   # if n > 0 and m > 0 and n > m:
+        for i in range(m, n + 1):
+            print(i," ",end = " ") 
+    else:
+        print("Podaj liczbę większą niż 0 !! ")
     return 0
 
 if __name__ == '__main__':
---
+++ python/zadanie3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    liczba = int(input("Podaj liczbę")
+    for i in range(0, liczba + 1):
+        print(i*i)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

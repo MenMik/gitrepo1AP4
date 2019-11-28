b91be676893fead4c8031f76d99da4b55832f5f4
MenMik <mikolaj.mendala@lo1.sandomierz.pl>
2019-11-28 12:34:13 +0100

    instrukcja warunkowa w c++

--- C++/Równanie.cpp
+++ C++/Równanie.cpp
@@ -9,7 +9,27 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    cout << "Witaj w C++";
     
+    int a;
+    int b;
+    float x;
+    cout << "Podaj liczbę a";
+    cin >> a;
+    cout << "Podaj liczbę b";
+    cin >> b;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "Podaj wiele rozwiązań";            
+            } else {
+                cout << " równianie sprzeczne"; 
+            }
+            
+    } else {
+        x = -b /(float)a;
+        cout << x;
+    }
 	return 0;
+    
 }
 

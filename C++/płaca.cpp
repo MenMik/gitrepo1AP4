d72239adfbbabb1f89f7a5eb5ddb4584cbe5e596
MenMik <mikolaj.mendala@lo1.sandomierz.pl>
2019-12-05 12:37:29 +0100

    wieksza c++

---
+++ C++/płaca.cpp
@@ -0,0 +1,21 @@
+/*
+ * płaca.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+	float a, b;
+    cout << " Podaj liczbę nadgodzin: " << endl;
+    cin >> a;
+    cout << " Podaj stawke za jedną godzine: " << endl;
+    cin >> b;
+    
+    if ( a <= 30 ) ;
+        b = (a * b)
+        
+	return 0;
+}
+
